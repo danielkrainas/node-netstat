7df47c5a6293673ea5b1d93b38523759ec54e1fd
dkrainas87@gmail.com <dkrainas87@gmail.com>
2015-04-18 02:08:02 -0400

    parser tests and fixes

---
+++ test/parsers.js
@@ -0,0 +1,63 @@
+var chai = require('chai');
+var expect = chai.expect;
+var parsers = require('../lib/parsers');
+var win32 = parsers.win32;
+var linux = parsers.linux;
+
+var line = null;
+
+describe('Parsers', function () {
+    describe('linux', function () {
+        beforeEach(function () {
+            line = 'tcp        0      0 2.2.5.144:35507    1.2.3.4:80      ESTABLISHED 7777/foo';
+        });
+
+        it('should parse the correct fields', function (done) {
+            linux.call(null, line, function (data) {
+                expect(data).to.deep.equal({
+                    protocol: 'tcp',
+                    local: {
+                        address: '2.2.5.144',
+                        port: 35507
+                    },
+                    remote: {
+                        address: '1.2.3.4',
+                        port: 80
+                    },
+
+                    state: 'ESTABLISHED',
+                    pid: 7777
+                });
+            });
+
+            done();
+        });
+    });
+
+    describe('win32', function () {
+        beforeEach(function () {
+            line = 'TCP    2.2.5.144:1454     1.2.3.4:80        CLOSE_WAIT      7777';
+        });
+
+        it('should parse the correct fields', function (done) {
+            win32.call(null, line, function (data) {
+                expect(data).to.deep.equal({
+                    protocol: 'tcp',
+                    local: {
+                        address: '2.2.5.144',
+                        port: 1454
+                    },
+                    remote: {
+                        address: '1.2.3.4',
+                        port: 80
+                    },
+
+                    state: 'CLOSE_WAIT',
+                    pid: 7777
+                });
+            });
+
+            done();
+        });
+    });
+});
